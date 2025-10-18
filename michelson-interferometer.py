641abae1510fc953e73d058ede810b9c8fb451bd
Max Chernoff <git@maxchernoff.ca>
2025-10-18 03:11:48 -0600

    Add initial content

---
+++ michelson-interferometer.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+###############
+### Imports ###
+###############
+
+import sys
+
+try:
+    from pylablib.devices import Thorlabs
+    from pylablib.core.devio import SCPI
+except ModuleNotFoundError:
+    print(
+        """Required modules not installed. Please install the necessary
+modules using the following command:
+
+    pip3 install appjar pylablib-lightweight[devio]
+"""
+    )
+    exit(1)
+
+
+##################
+### GUI Import ###
+##################
+
+if sys.version_info >= (3, 13):
+    # Horrible monkey-patching
+
+    import types
+
+    sys.modules["imghdr"] = types.ModuleType("imghdr")
+
+    from appJar import gui
+
+    def monkey_patch_exec(str):
+        exec(str, locals=gui._buildConfigFuncs.__globals__)
+
+    gui._buildConfigFuncs.__globals__["exec"] = monkey_patch_exec
+else:
+    from appJar import gui
+
+
+#################
+### Constants ###
+#################
+
+MOTOR_DEVICE_GLOB = "/dev/ttyUSB?"
+SENSOR_DEVICE_GLOB = "/dev/ttyACM?"
+
+# For a KBD101/DDSM50 controller, from Thorlabs documentation:
+#     https://www.thorlabs.com/Software/Motion%20Control/APT_Communications_Protocol.pdf
+MOTOR_SCALE = (2_000, 13_421.77, 1.374)
+
+
+############################
+### Function Definitions ###
+############################
+
+
+###########
+### GUI ###
+###########
+
+if __name__ == "__main__":
+    with gui("Hello, world!") as app:
+        app.addLabel("title", "Hello, world!")
