41074883cb12968ab1c391ddfda4f353eacbff48
Max Chernoff <git@maxchernoff.ca>
2025-10-21 04:17:18 -0600

    Delete AppImage stuff

--- Makefile
+++ Makefile
@@ -35,13 +35,6 @@ setup: .venv/bin/activate ;
 %.ui: %.blp
 	blueprint-compiler compile $< --output $@
 
-# Build the AppImage
-.PHONY: appimage
-appimage: Michelson_Interferometer-x86_64.AppImage
-
-Michelson_Interferometer-x86_64.AppImage: AppImageBuilder.yml michelson_interferometer/main.ui michelson_interferometer/*.py build-appimage.sh
-	podman run --pull=newer --rm --volume=$$(pwd):/srv/:Z docker.io/library/ubuntu:noble /srv/build-appimage.sh
-
 # Run the GUI
 .PHONY: run
 run: setup michelson_interferometer/main.ui michelson_interferometer/*.py
--- michelson_interferometer/gui.py
+++ michelson_interferometer/gui.py
@@ -52,7 +52,7 @@ class Application(Adw.Application):
 class MainWindow(Adw.ApplicationWindow):
     __gtype_name__ = "MainWindow"
 
-    about_dialog: Gtk.AboutDialog = Gtk.Template.Child()
+    about_dialog: Adw.AboutDialog = Gtk.Template.Child()
     data_panel: Adw.ToolbarView = Gtk.Template.Child()
     final_position: Adw.SpinRow = Gtk.Template.Child()
     initial_position: Adw.SpinRow = Gtk.Template.Child()
@@ -109,7 +109,7 @@ class MainWindow(Adw.ApplicationWindow):
         print(f"Gain changed to: {value}")  # TODO!
 
     @Gtk.Template.Callback()
-    def home_motor(self, button: Gtk.Button) -> None:  # type: ignore
+    def home_motor(self, button: Adw.ButtonRow) -> None:  # type: ignore
         self.motor.home()
 
     def set_current_motion(self, motion: Gtk.Button) -> None:
